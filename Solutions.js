56b63705630f405158069f962792e7073a593ecc
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-09-03 17:11:03 -0700

    update

--- Solutions.js
+++ Solutions.js
@@ -502,3 +502,35 @@ var characterReplacement = function(s, k) {
     
     return res;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var diameterOfBinaryTree = function(root) {
+    var max = 0;
+    var dfs = (node) => {
+        if(!node) return 0;
+        if(!node.left && !node.right) return 0;
+        
+        let left = dfs(node.left);
+        let right = dfs(node.right);
+        
+        if(node.left && node.right)
+            if(max < (left + right + 2)) max = left + right + 2;
+        if(max < (1 + Math.max(left, right))) max = 1 + Math.max(left, right);
+        
+        return 1 + Math.max(left, right);
+    }
+    dfs(root);
+    
+    return max;
+};
