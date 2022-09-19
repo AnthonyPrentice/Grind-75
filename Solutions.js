df9a0c42b0b09dedbf58862b5f99e45e71bdb007
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-09-19 12:27:21 -0700

    updated

--- Solutions.js
+++ Solutions.js
@@ -964,3 +964,21 @@ var countBits = function(n) {
     
     return ret;
 };
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+var reverseBits = function(n) {
+    let ret = 0;
+    
+    for(let i = 0; i < 32; i++){ 
+        let last = n & 1;
+        last = last << (31 - i);
+        ret = ret | last;
+        ret = ret >>> 0;
+        n = n >> 1;
+    }
+    
+    return ret;
+};
