0f66b66168d884e11d8142104e627ea6cdecc550
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-09-19 14:08:14 -0700

    updated

--- Solutions.js
+++ Solutions.js
@@ -982,3 +982,16 @@ var reverseBits = function(n) {
     
     return ret;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let res = nums.length;
+    
+    for(let i = 0; i < nums.length; i++)
+        res = res ^ i ^ nums[i];
+        
+    return res;
+};
