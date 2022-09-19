3536c3e81105054d3e0773b8eb5e0ab9c3c9c639
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-09-18 18:43:52 -0700

    updated

--- Solutions.js
+++ Solutions.js
@@ -947,3 +947,20 @@ var kClosest = function(points, k) {
     }
     return ret;
 };
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var countBits = function(n) {
+    let ret = [0]
+    let offset = 1;
+    
+    for(let i = 1; i <= n; i++){
+        if(i == offset*2)
+            offset *= 2;
+        ret[i] = ret[i - offset] + 1;
+    }
+    
+    return ret;
+};
