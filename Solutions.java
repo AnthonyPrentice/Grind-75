afe2d54ebd38cd659eb492e02faee7cee7a16224
AnthonyPrentice <60078664+AnthonyPrentice@users.noreply.github.com>
2022-10-17 11:18:20 -0700

    updated

--- Solutions.java
+++ Solutions.java
@@ -27,3 +27,32 @@ class Solution {
         return ret;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
+        ListNode left = dummy;
+        ListNode right = head;
+        
+        for(int i = 1; i < n; i++)
+            right = right.next;
+        
+        while(right.next != null) {
+            left = left.next;
+            right = right.next;
+        }
+        
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
